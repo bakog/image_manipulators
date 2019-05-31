f3bc8cde8266441237311ad2a50e4f71d665ad6b
bakog <bako.gabor@gmail.com>
2019-05-31 14:28:53 +0200

    test_clean_file_name_function test_get_exif_info_function

--- test_jpg_collector_by_exif.py
+++ test_jpg_collector_by_exif.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import time
 
-from jpg_collector_by_exif import check_dir_exists, create_no_exif_data_dir, clean_file_name
+from jpg_collector_by_exif import check_dir_exists, create_no_exif_data_dir, clean_file_name, get_exif_info, set_exif_info_in_filename
 
 working_dir = os.getcwd()
 temp_root_dir_for_tests = os.path.join(working_dir, "tmp")
@@ -13,6 +13,10 @@ source_dir_for_tests = os.path.join(temp_root_dir_for_tests, "source")
 target_dir_for_tests = os.path.join(temp_root_dir_for_tests, "target")
 sample_dir = os.path.join(working_dir, "sample_data")
 dest_dir_no_exif = target_dir_for_tests + "/no_exif_data"
+
+picture_without_exif_1 = os.path.join(sample_dir, "pictures_without_exif/DSCN0017.JPG")
+picture_with_exif_in_filename = os.path.join(sample_dir, "pictures_with_exif/DSCF0922_2006_01_01_09_30_29.JPG")
+picture_with_exif_not_in_filename = os.path.join(sample_dir, "pictures_with_exif/DSCF0791.JPG")
 
 
 def setup():
@@ -27,9 +31,6 @@ def teardown():
 
 def test_check_dir_exists_function():
     assert check_dir_exists(temp_root_dir_for_tests) == True
-
-
-def test_check_dir_no_exists_function():
     assert check_dir_exists("/ez_nem_letezik") == False
 
 
@@ -39,11 +40,24 @@ def test_create_no_exif_data_dir_function():
 
 def test_clean_file_name_function():
     # with space
-    assert clean_file_name("ez egy rossz fajlnev  ") == "ez_egy_rossz_fajlnev"
+    assert clean_file_name("   ez egy rossz fajlnev  ") == "ez_egy_rossz_fajlnev"
 
     # with non alfanumeric char
     assert clean_file_name("ez egy@rossz:fajlnev  ") == "ez_egyrosszfajlnev"
 
     # with hungarian special chars
-    assert clean_file_name("és ő rossz fájlnév  ") == "es_o_rossz_fajlnev"
+    assert clean_file_name("és ő rossz fájlnév  öüóőúéáűíÖÜÓŐÚÉÁŰÍ") == "es_o_rossz_fajlnev_ouooueauiouooueaui"
+
+    # with multiple _
+    assert clean_file_name("és ő rossz____ fájlnév__  öüóőúéáűíÖÜÓŐÚÉÁŰÍ___") == "es_o_rossz_fajlnev_ouooueauiouooueaui"
+
+def test_get_exif_info_function():
+
+    assert get_exif_info(picture_without_exif_1) == None
+    assert get_exif_info(picture_with_exif_in_filename) == "2006_01_01_09_30_29"
+
+def teyt_set_exif_info_in_filename():
+    assert get_exif_info(picture_with_exif_in_filename) == "DSCF0922_2006_01_01_09_30_29.JPG"
+    assert get_exif_info(picture_with_exif_not_in_filename) == "DSCF0791_2005_08_13_14_23_06.JPG"
+
 
