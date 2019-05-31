4860c1c7924d6bd01d3e545072ff772b1cac338e
bakog <bako.gabor@gmail.com>
2019-05-31 14:30:52 +0200

    init version

--- image_exif_data.py
+++ image_exif_data.py
@@ -8,7 +8,16 @@ import time
 import hashlib
 
 
-with open("/home/bakog/Dropbox/phyton/image_renamer_by_exif/teszt_kepek/kepek_2/vmps.jpg", 'rb') as image_file:
+with open("/home/bakog/Dropbox/phyton/image_renamer_by_exif/sample_data/pictures_without_exif/DSCN0017.JPG", 'rb') as image_file:
     my_image = Image(image_file)
     image_date = my_image.get('datetime_original', None)
-    print(image_date)
+    image_date_converted = image_date.replace(":", '_').replace(" ", '_')
+
+    print(len(image_date_converted))
+
+    print(image_date_converted.strip('\n\l'))
+
+    print(image_date_converted.replace(chr(0), ""))
+
+    for c in image_date_converted:
+        print(ord(c))
