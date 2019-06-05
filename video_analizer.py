8780f8f2fb76fb446a5a6d9010bc1f613b7a5aa4
bakog <bako.gabor@gmail.com>
2019-06-05 13:53:53 +0200

    get_exif_info_for_video funkció javítás -, :, szóköz érzékelés finomítása

--- video_analizer.py
+++ video_analizer.py
@@ -17,20 +17,26 @@ def get_exif_info_for_video(filename_with_path: str):
     process = os.popen('exiftool ' + filename_with_path.replace(" ", "\ ") + ' | grep -e "Create Date" -e "Creation time" -e "File Access Date/Time" -e "File Modification Date/Time"', 'r', 1)
     for item in process:
         datum = item.split(":", 1)[1].strip()
-        #print(datum[0:4])
-        if datum[0:4] != "0000" and int(datum[0:4]) < 2019:
-            #print("Dátum hossza: ", len(datum))
-            print("hozzáadom ", datum )
-            dates.append(datum)
+        datum = datum.replace(":", "_").replace(" ", "_").replace("-", "_")
+
+        #print(datum[0:4], datum.split("_")[0], filename)
+        datum_kezdet = datum.split("_")[0]
+        try:
+            if datum_kezdet != "0000" and int(datum_kezdet) < 2019:
+                #print("Dátum hossza: ", len(datum))
+                #print("hozzáadom ", datum )
+                dates.append(datum)
+        except:
+            print("Oppá, valami gond van, az adatok átalakításával...")
 
     if len(dates):
         dates.sort()
         #print(dates)
         #print(dates[1], min(dates))
         #print(min(dates))
-        return min(dates).strip().replace(":", "_").replace(" ", "_")[0:19]
+        return min(dates).strip()[0:19]
     else:
-        print("Nincs exif információ")
+        #print("Nincs exif információ")
         return None
 
 
@@ -43,13 +49,13 @@ dest_dir = "./videos"
 
 dest_dir_no_exif = create_no_exif_data_dir(dest_dir)
 
-file_hash_in_dest_dir = open_image_hash_from_file()
+file_hash_in_dest_dir = open_file_hash_from_file()
 if not file_hash_in_dest_dir:
     info_message("A meglévő fájlok ellenőrzése a cél könyvtárban....")
     info_message("Sok kép esetén a folyamat hosszabb ideig tart, várjon türelemmel...")
-    file_hash_in_dest_dir = get_image_hash_in_dir(dest_dir)
+    file_hash_in_dest_dir = get_file_hash_in_dir(dest_dir)
 
-save_image_hash_to_file(file_hash_in_dest_dir)
+save_file_hash_to_file(file_hash_in_dest_dir)
 
 file_counter_for_same_name = 0
 file_counter_dest_dir_no_exif = 0
@@ -67,10 +73,10 @@ for root, dirs, files in os.walk(start_dir, topdown=True):
         filename, filename_ext = os.path.splitext(name)
         filename = clean_file_name(filename)
 
-        image_date_converted = get_exif_info_for_video(original_name_with_path)
+        video_date_converted = get_exif_info_for_video(original_name_with_path)
 
-        if image_date_converted:
-            filename = set_exif_info_in_filename(image_date_converted, filename)
+        if video_date_converted:
+            filename = set_exif_info_in_filename(video_date_converted, filename)
             target_dir = dest_dir
         else:
             target_dir = dest_dir_no_exif
