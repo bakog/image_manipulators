79cb1a5842d61db4fc199e682a83748c967f33ee
bakog <bako.gabor@gmail.com>
2019-06-05 13:56:06 +0200

    pep8 javítások

--- video_analizer.py
+++ video_analizer.py
@@ -19,71 +19,75 @@ def get_exif_info_for_video(filename_with_path: str):
         datum = item.split(":", 1)[1].strip()
         datum = datum.replace(":", "_").replace(" ", "_").replace("-", "_")
 
-        #print(datum[0:4], datum.split("_")[0], filename)
+        # print(datum[0:4], datum.split("_")[0], filename)
         datum_kezdet = datum.split("_")[0]
         try:
             if datum_kezdet != "0000" and int(datum_kezdet) < 2019:
-                #print("Dátum hossza: ", len(datum))
-                #print("hozzáadom ", datum )
+                # print("Dátum hossza: ", len(datum))
+                # print("hozzáadom ", datum )
                 dates.append(datum)
         except:
             print("Oppá, valami gond van, az adatok átalakításával...")
 
     if len(dates):
         dates.sort()
-        #print(dates)
-        #print(dates[1], min(dates))
-        #print(min(dates))
+        # print(dates)
+        # print(dates[1], min(dates))
+        # print(min(dates))
         return min(dates).strip()[0:19]
     else:
-        #print("Nincs exif információ")
+        # print("Nincs exif információ")
         return None
 
 
-#
-# start_dir = get_dir("forrás")
-# dest_dir = get_dir("cél")
+def main():
 
-start_dir = "./sample_data/videos"
-dest_dir = "./videos"
+    #
+    # start_dir = get_dir("forrás")
+    # dest_dir = get_dir("cél")
 
-dest_dir_no_exif = create_no_exif_data_dir(dest_dir)
+    start_dir = "./sample_data/videos"
+    dest_dir = "./videos"
 
-file_hash_in_dest_dir = open_file_hash_from_file()
-if not file_hash_in_dest_dir:
-    info_message("A meglévő fájlok ellenőrzése a cél könyvtárban....")
-    info_message("Sok kép esetén a folyamat hosszabb ideig tart, várjon türelemmel...")
-    file_hash_in_dest_dir = get_file_hash_in_dir(dest_dir)
+    dest_dir_no_exif = create_no_exif_data_dir(dest_dir)
 
-save_file_hash_to_file(file_hash_in_dest_dir)
+    file_hash_in_dest_dir = open_file_hash_from_file()
+    if not file_hash_in_dest_dir:
+        info_message("A meglévő fájlok ellenőrzése a cél könyvtárban....")
+        info_message("Sok kép esetén a folyamat hosszabb ideig tart, várjon türelemmel...")
+        file_hash_in_dest_dir = get_file_hash_in_dir(dest_dir)
 
-file_counter_for_same_name = 0
-file_counter_dest_dir_no_exif = 0
-file_counter_dest_dir = 0
-file_counter_bad_image = 0
-file_count_deleted = 0
+    save_file_hash_to_file(file_hash_in_dest_dir)
 
-dates = list()
+    file_counter_for_same_name = 0
+    file_counter_dest_dir_no_exif = 0
+    file_counter_dest_dir = 0
+    file_counter_bad_image = 0
+    file_count_deleted = 0
 
-for root, dirs, files in os.walk(start_dir, topdown=True):
-    for name in files:
+    dates = list()
 
-        original_name = name
-        original_name_with_path = os.path.join(root, name)
-        filename, filename_ext = os.path.splitext(name)
-        filename = clean_file_name(filename)
+    for root, dirs, files in os.walk(start_dir, topdown=True):
+        for name in files:
 
-        video_date_converted = get_exif_info_for_video(original_name_with_path)
+            original_name = name
+            original_name_with_path = os.path.join(root, name)
+            filename, filename_ext = os.path.splitext(name)
+            filename = clean_file_name(filename)
 
-        if video_date_converted:
-            filename = set_exif_info_in_filename(video_date_converted, filename)
-            target_dir = dest_dir
-        else:
-            target_dir = dest_dir_no_exif
-            error_message("\nAz EXIF információ nem elérhető... " + original_name)
+            video_date_converted = get_exif_info_for_video(original_name_with_path)
 
-        new_name = filename + filename_ext
+            if video_date_converted:
+                filename = set_exif_info_in_filename(video_date_converted, filename)
+                target_dir = dest_dir
+            else:
+                target_dir = dest_dir_no_exif
+                error_message("\nAz EXIF információ nem elérhető... " + original_name)
 
-        print(new_name)
+            new_name = filename + filename_ext
+
+            print(new_name)
 
 
+if __name__ == "__main__":
+    main()
