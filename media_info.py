f83a2717635858ed43460569970ad6588ee2c34d
bakog <bako.gabor@gmail.com>
2019-06-05 13:33:28 +0200

    teszt file form media info

--- media_info.py
+++ media_info.py
@@ -18,7 +18,7 @@ print(os.getcwd())
 filename ="P1010379.MP4"
 
 print(filepath)
-process = Popen(['mediainfo', '--Inform="[General]"', filename], stdout=PIPE, stderr=PIPE)
+process = Popen(['mediainfo', filename], stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
 process.wait()
 print(stdout)
@@ -45,3 +45,17 @@ for item in info:
 print(info_dict['Encoded date'])
 
 #print(sh.mediainfo('--Inform="Video;%DisplayAspectRatio%"',filename))
+
+info = list(sh.exiftool(filename).split('\n'))
+print(info)
+
+
+process = os.popen('exiftool '+ filename + ' | grep "Create Date"', 'r', 1)
+
+for item in process:
+    egysor = item.split(":", 1)[1]
+    print("sor:", type(egysor), egysor)
+
+
+sor = "Media Create Date               : 2017:02:09 08:25:01"
+print(sor.split(": ", 1)[1].replace(":", "_").replace(" ", "_"))
